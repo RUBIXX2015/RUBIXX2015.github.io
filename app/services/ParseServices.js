82374b34a4c41845f1205c4e148d654e9ea3219e
RUBIXX2015 <rubixx2015@gmail.com>
2015-12-16 19:30:19 -0500

    Update ParseServices.js

--- app/services/ParseServices.js
+++ app/services/ParseServices.js
@@ -2,6 +2,6 @@ angular.module('ParseServices', [])
 .factory('ParseSDK', function() {
 
   // pro-tip: swap these keys out for PROD keys automatically on deploy using grunt-replace
-  Parse.initialize("URmLhPy0JkjYYTsj8gb0ovzwgBZZyE5rdmAfzJn1", "kfgeDrMXnqNvFX0fxZeRJcWgkAq7oAlZO4GV1Pgc");
+  Parse.initialize("PgiXb5njv7cEQmx3VpfHW5q7bZ8Ix55cLJv6ea0E", "6ZvwPAvcFFTKmRhSVJZsaMomBLZDVP37NLOf01tw");
 
 });
